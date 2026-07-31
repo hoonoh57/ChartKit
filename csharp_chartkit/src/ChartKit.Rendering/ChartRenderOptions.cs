7ef1dac054c682be96a83fa9585cc9b69e165716
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-08-01 03:15:00 +0900

    Add chart date-boundary render option

--- csharp_chartkit/src/ChartKit.Rendering/ChartRenderOptions.cs
+++ csharp_chartkit/src/ChartKit.Rendering/ChartRenderOptions.cs
@@ -2,7 +2,8 @@ namespace ChartKit.CSharp.Rendering;
 
 public sealed record ChartRenderOptions(
     bool ShowText = true,
-    bool ShowAxes = true)
+    bool ShowAxes = true,
+    bool ShowDateBoundaries = true)
 {
     public static ChartRenderOptions Default { get; } = new();
 
