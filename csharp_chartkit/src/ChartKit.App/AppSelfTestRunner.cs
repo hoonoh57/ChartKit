b1da98bad5b41e3cddc516507bedd046fe8f019e
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-08-02 08:44:52 +0900

    Run VWAP app self-test

--- csharp_chartkit/src/ChartKit.App/AppSelfTestRunner.cs
+++ csharp_chartkit/src/ChartKit.App/AppSelfTestRunner.cs
@@ -5,6 +5,7 @@ internal static class AppSelfTestRunner
     public static async Task<int> RunAsync(AppOptions options)
     {
         ArgumentNullException.ThrowIfNull(options);
+        await VwapModuleAppVerification.RunAsync(options.Timeframe);
         await DisparityModuleAppVerification.RunAsync(options.Timeframe);
         await ObvModuleAppVerification.RunAsync(options.Timeframe);
         await JmaModuleAppVerification.RunAsync(options.Timeframe);
