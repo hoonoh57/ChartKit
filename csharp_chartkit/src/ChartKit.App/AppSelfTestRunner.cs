4702a73f9fa08f5921fac759bece268813b31191
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-08-02 02:29:57 +0900

    Run SuperTrend app integration self-test

--- csharp_chartkit/src/ChartKit.App/AppSelfTestRunner.cs
+++ csharp_chartkit/src/ChartKit.App/AppSelfTestRunner.cs
@@ -5,6 +5,7 @@ internal static class AppSelfTestRunner
     public static async Task<int> RunAsync(AppOptions options)
     {
         ArgumentNullException.ThrowIfNull(options);
+        await SuperTrendModuleAppVerification.RunAsync(options.Timeframe);
         await MacdModuleAppVerification.RunAsync(options.Timeframe);
         await MacdPanelAppVerification.RunAsync(options.Timeframe.ToString());
         return await ConsoleModes.RunSelfTestAsync(options);
